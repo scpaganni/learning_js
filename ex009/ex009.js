35a3689637aa887b9bccd7ab512850554b2c698d
scpaganni <scpaganni@gmail.com>
2022-07-22 21:48:26 -0400

    Condicional Simples em JS

---
+++ ex009/ex009.js
@@ -0,0 +1,6 @@
+var vel = 70
+console.log(`A velocidade do seu carro é ${vel}km`)
+if (vel >= 70) { // Condição Simples
+    console.log(`Você está acima da velocidade permitida, MULTADO`)
+}
+console.log(`Dirija sempre usando cinto de segurança`)
