593d70d27014a73b5235f99fef8169292657888b
scpaganni <scpaganni@gmail.com>
2022-08-02 20:31:52 -0400

    Estruturas de Repetição

---
+++ ex013/script01.js
@@ -0,0 +1,15 @@
+// O while é uma estrutura de repetição com com teste lógico no início
+
+var contador = 1
+while (contador <= 20) {
+  console.log(`Numero ${contador}`)
+  contador++
+}
+
+// O do while é uma estrutura de repetição com teste lógico no final
+
+var contador = 5
+do {
+  console.log(`Número ${contador}`)
+  contador++
+} while (contador <= 30)
---
+++ ex013/script02.js
@@ -0,0 +1,5 @@
+// Estrutura de repetição com variável de controle utilizando for
+
+for (var num = 1; num <= 10; num++) {
+  console.log(`Número ${num}`)
+}
